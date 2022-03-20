d52a5179b232628e91733516da69e22e2c9d7c21
GuiSilva77 <guilherme92325@gmail.com>
2022-03-20 17:18:12 -0300

    ED - exercício 20-03

--- aula 07-03/atividade-quadrado.c
+++ aula 07-03/atividade-quadrado.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINHA 3
-#define COLUNA 3
+#define LINHA 4
+#define COLUNA 4
 
 int busca(int mat[LINHA][COLUNA], int chave, int lin, int col);
 void mostrar_matriz(int mat[LINHA][COLUNA]);
@@ -10,7 +10,7 @@ int validar_quadrado(int mat[LINHA][COLUNA]);
 
 int main()
 {
-  int matriz[LINHA][COLUNA], i, j;  
+  int matriz[LINHA][COLUNA], i, j;
 
     for(i = 0; i < LINHA; i++)
     {
@@ -26,14 +26,14 @@ int main()
             }
         }
     }
-    
+
     mostrar_matriz(matriz);
-    
+
     if (validar_quadrado(matriz))
         printf("Os valores inseridos formam um quadrado magico.\n");
     else
         printf("Os valores inseridos NAO formam um quadrado magico.\n");
-    
+
     system("pause");
     return 0;
 }
@@ -52,7 +52,7 @@ int busca(int mat[LINHA][COLUNA], int chave, int lin, int col)
             }
         }
     }
-    
+
     return 0;
 }
 
@@ -74,14 +74,14 @@ int validar_quadrado(int mat[LINHA][COLUNA])
 {
     int soma = 0, soma_diagonalP = 0;
     int i, j;
-    
+
     /*
         Para avaliar o quadrado mágico, pensei na seguinte solução:
         - são 9 linhas, 9 colunas e 2 diagonais;
-        - se todas elas são iguais então a soma dos valores é igual a multilicação da 
+        - se todas elas são iguais então a soma dos valores é igual a multilicação da
           diagonal principal pelo numero de linhas;
     */
-    
+
     //soma dos valores
     for(i = 0; i < LINHA; i++)
     {
@@ -89,7 +89,7 @@ int validar_quadrado(int mat[LINHA][COLUNA])
         {
             if (i == j)
                 soma_diagonalP += mat[i][j];
-                
+
             soma += mat[i][j];
         }
     }
